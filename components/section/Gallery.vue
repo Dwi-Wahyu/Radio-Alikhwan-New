a65b7688d43ea20104434f7ee71322964fc8bbcc
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2025-03-07 04:15:41 +0800

    fix gallery

--- components/section/Gallery.vue
+++ components/section/Gallery.vue
@@ -9,94 +9,50 @@
       <Swiper
         @swiper="onSwiper"
         @slide-change="onSlideChange"
-        :slides-per-view="3"
+        :slides-per-view="1"
+        :breakpoints="breakpoints"
       >
         <SwiperSlide v-for="(imageUrl, imageIdx) in dataGaleri" :key="imageIdx">
-          <img
-            :src="`${baseURL}/galeri/${imageUrl.path}`"
-            class="max-h-20 sm:max-h-56"
-            alt=""
-          />
+          <div class="flex items-center max-h-56 justify-center">
+            <img
+              :src="`${baseURL}/galeri/${imageUrl.path}`"
+              class="max-h-96"
+              alt=""
+            />
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
 
-    <!-- <div v-if="!dataGaleri.length">
+    <div v-if="!dataGaleri.length">
       <h1 class="mb-4">Belum ada foto di galeri</h1>
     </div>
-
-    <Carousel
-      v-if="dataGaleri"
-      class="block sm:hidden"
-      v-bind="mobileCarouselConfig"
-    >
-      <Slide v-for="(imageUrl, imageIdx) in dataGaleri" :key="imageIdx">
-        <img
-          :src="`${baseURL}/galeri/${imageUrl.path}`"
-          class="max-w-96"
-          alt=""
-        />
-      </Slide>
-
-      <template #addons>
-        <div class="mt-5">
-          <Pagination />
-        </div>
-      </template>
-    </Carousel>
-
-    <Carousel v-if="dataGaleri" class="hidden sm:block" v-bind="carouselConfig">
-      <Slide v-for="(imageUrl, imageIdx) in dataGaleri" :key="imageIdx">
-        <img
-          :src="`${baseURL}/galeri/${imageUrl.path}`"
-          class="max-w-96 max-h-[353px]"
-          alt=""
-        />
-      </Slide>
-
-      <template #addons>
-        <div class="mt-5">
-          <Pagination />
-        </div>
-      </template>
-    </Carousel> -->
   </div>
 </template>
 
 <script setup>
-// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
-// import "vue3-carousel/carousel.css";
-
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-// import { Carousel, Slide, Pagination } from "vue3-carousel";
-
 import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination, A11y, Navigation, Scrollbar } from "swiper/modules";
+
+const breakpoints = {
+  640: {
+    slidesPerView: 1,
+  },
+  768: {
+    slidesPerView: 3,
+  },
+};
 
 const runtimeConfig = useRuntimeConfig();
 const { baseURL } = runtimeConfig.public.axios;
 const axios = useAxios();
 
-const dataGaleri = ref([]);
-
-// const carouselConfig = {
-//   itemsToShow: 3.5,
-// };
-
-// const mobileCarouselConfig = {
-//   itemsToShow: 1,
-// };
-
-// const galleryImage = [
-//   "/gallery/image 10.png",
-//   "/gallery/image 11.png",
-//   "/gallery/image 13.png",
-//   "/gallery/image 14.png",
-//   "/gallery/image 7.png",
-// ];
+const dataGaleri = ref([Pagination, A11y, Navigation]);
 
 function onSwiper(swiper) {
   console.log(swiper);
